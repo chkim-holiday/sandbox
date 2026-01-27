0bb813b3fbdfac48d5ee2daa83176725e025d60c
ChanghyeonKim93 <hyun91015@gmail.com>
2026-01-27 16:46:04 +0900

    verbose ptp client

--- serial_communicator/ethernet_ptp_test/run_ptp_client.sh
+++ serial_communicator/ethernet_ptp_test/run_ptp_client.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-IFACE=${1}  
-# if not provided, show error message and exit
-
+IFACE=${1}
 if [ -z "$1" ]; then
     echo "Error: Need interface name as argument."
     echo "Usage: $0 <interface name>"
@@ -10,4 +8,4 @@ fi
 
 echo "Network Interface: $IFACE"
 echo "Local PTP Client Mode"
-sudo ptp4l -i "$IFACE" -S -s --step_threshold=1 --first_step_threshold=0
+sudo ptp4l -i "$IFACE" -m -S -s --step_threshold=1 --first_step_threshold=0
